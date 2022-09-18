9f45fe8c909d50dadf96da5542076e3841528379
Maruchan-debug <78717496+Maruchan-debug@users.noreply.github.com>
2022-09-17 17:52:53 -0700

    MCasillas_Unity_Review_Lesson
    It took me a while because I accidentally mixed up the file paths when creating the repository. I fixed it instead by redoing the project, and once completed then I dragged the files over into github. Better late than never I guess.

---
+++ Assets/Scripts/Drift.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drift : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    public enum DriftDirection
+    {
+        Left = -1,
+        Right = 1
+    }
+
+    public DriftDirection driftDirection = DriftDirection.Left;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        switch (driftDirection)
+        {
+            case DriftDirection.Left:
+                transform.Translate(Vector3.left * Time.deltaTime * speed);
+                break;
+            case DriftDirection.Right:
+                transform.Translate(Vector3.right * Time.deltaTime * speed);
+                break;
+
+        }
+
+        if (transform.position.x < -80 || transform.position.x > 80)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            GameObject child = collision.gameObject;
+            child.transform.SetParent(gameObject.transform);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            GameObject child = collision.gameObject;
+            child.transform.SetParent(null);
+        }
+    }
+}
